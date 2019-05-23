459464ad3ff563bd310ab2c4db363d07100f893d
fadli7 <fadlifarham10@gmail.com>
2019-05-23 10:11:35 +0700

    update apriori

--- main.py
+++ main.py
@@ -6,52 +6,37 @@ import pandas as pd
 from apyori import apriori  
 import math
 
-#membaca file
-datafile = pd.read_csv("data/dataset2.csv", index_col=False, squeeze=True)
-#print(datafile)
+# MARK -> Read File Dataset
+datafile = pd.read_csv("data/apriori.csv", index_col=False, squeeze=True)
 
+# MARK -> Show Dataset
 print(datafile.head())
-# print(datafile.dropna())
 
-#use apriori
+# MARK -> Preprocessing
 records = []  
-for i in range(0, 5):
+for i in range(0, datafile.shape[0]):
     temp = []
-    for j in range(0, 4):
+    for j in range(0, datafile.shape[1]):
         if str(datafile.values[i, j]) != 'nan':
             temp.append(str(datafile.values[i, j]))
     records.append(temp)
 
-print(records)
-
-#applying apriori
+# MARK -> Do Apriori
 association_rules = apriori(records, min_support=0.3, min_confidence=0.8, use_colnames=True, min_len=2)
-association_results = list(association_rules) 
-# print(association_results[0])
+association_results = list(association_rules)
 
+# MARK -> Show Output
+print()
 for item in association_results:
-    # print(item)
-    #print(item[0])
-    # print(item[1])
-    # print(len(item[0]))
+    itemset = [x for x in item[0]]
+    order_statistics = item[2][0]
 
-    # first index of the inner list
-    # Contains base item and add item
-    pair = item[0] 
-    items = [x for x in pair]
-    if (len(pair) == 1):
-        print("Rule: " + items[0])
-    elif (len(pair) == 2):    
-        print("Rule: " + items[0] + " -> " + items[1])
-    elif (len(pair) == 3):
-        print("Rule: " + items[0] + " -> " + items[1] + " & " + items[2]) 
 
-    #second index of the inner list
-    print("Support: " + str(item[1]))
-
-    #third index of the list located at 0th
-    #of the third index of the inner list
-
-    print("Confidence: " + str(item[2][0][2]))
-    print("Lift: " + str(item[2][0][3]))
-    print("=====================================")
+    for i in range(0, len(itemset)):
+        if i == 0:
+            print("Rule :", itemset[i], end=" ")
+        else :
+            print("->", itemset[i], end=" ")
+    print()
+    print("Confidence : ", order_statistics.confidence)
+    print("=====================")
